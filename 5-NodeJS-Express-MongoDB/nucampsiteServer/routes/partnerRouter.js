e1e6385c22802ae36125772d51ca5a0c0b424b25
DJ <daniel.henderson7032@gmail.com>
2020-10-30 20:06:21 -0700

    Cookies

--- 5-NodeJS-Express-MongoDB/nucampsiteServer/routes/partnerRouter.js
+++ 5-NodeJS-Express-MongoDB/nucampsiteServer/routes/partnerRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Partner = require('../models/partner');
 
 const partnerRouter = express.Router();
 
--- 5-NodeJS-Express-MongoDB/nucampsiteServer/routes/promotionRouter.js
+++ 5-NodeJS-Express-MongoDB/nucampsiteServer/routes/promotionRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Promotion = require('../models/promotion');
 
 const promotionRouter = express.Router();
 
