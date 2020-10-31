8bc072c8ed5d1460355a04c0154ec979ddb3b960
DJ <daniel.henderson7032@gmail.com>
2020-10-31 09:44:44 -0700

    Week 3 Workshop Assignment

--- 5-NodeJS-Express-MongoDB/nucampsiteServer/routes/users.js
+++ 5-NodeJS-Express-MongoDB/nucampsiteServer/routes/users.js
@@ -6,9 +6,14 @@ const authenticate = require('../authenticate');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
-});
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function (req, res, next) {
+    User.find()
+      .then(users => {
+        res.statusCode=200;
+        res.json(users);
+      })
+      .catch(err => next(err));
+  });
 
 router.post('/signup', (req, res) => {
     User.register(
