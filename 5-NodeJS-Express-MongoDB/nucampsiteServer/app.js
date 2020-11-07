03bc2ff5d2609a2f0e8b486c469fa329ba6a3932
DJ <daniel.henderson7032@gmail.com>
2020-11-07 10:02:20 -0800

    week 4 workshop

--- 5-NodeJS-Express-MongoDB/nucampsiteServer/app.js
+++ 5-NodeJS-Express-MongoDB/nucampsiteServer/app.js
@@ -1,40 +1,40 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-const passport = require('passport');
-const config = require('./config');
+const createError=require('http-errors');
+const express=require('express');
+const path=require('path');
+const logger=require('morgan');
+const mongoose=require('mongoose');
+const passport=require('passport');
+const config=require('./config');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const campsiteRouter = require('./routes/campsiteRouter');
-const partnerRouter = require('./routes/partnerRouter');
-const promotionRouter = require('./routes/promotionRouter');
-const uploadRouter = require('./routes/uploadRouter');
-
-const mongoose = require('mongoose');
-
-const url = config.mongoUrl;
-const connect = mongoose.connect(url, {
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
+const url=config.mongoUrl;
+const connect=mongoose.connect(url, {
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
-connect.then(() => console.log('Connected correctly to server'), 
-    err => console.log(err)
+connect.then(() => console.log('Connected correctly to server'),
+  err => console.log(err)
 );
 
-var app = express();
+const indexRouter=require('./routes/index');
+const usersRouter=require('./routes/users');
+const campsiteRouter=require('./routes/campsiteRouter');
+const promotionRouter=require('./routes/promotionRouter');
+const partnerRouter=require('./routes/partnerRouter');
+const uploadRouter=require('./routes/uploadRouter');
+const favoriteRouter=require('./routes/favoriteRouter');
+
+const app=express();
 
 // Secure traffic only
 app.all('*', (req, res, next) => {
   if (req.secure) {
     return next();
   } else {
-      console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
-      res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
+    console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
+    res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
   }
 });
 
@@ -44,9 +44,7 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser('12345-67890-09876-54321'));
-
+app.use(express.urlencoded({extended: false}));
 
 app.use(passport.initialize());
 
@@ -59,21 +57,22 @@ app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
 app.use('/imageUpload', uploadRouter);
+app.use('/favorites', favoriteRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message=err.message;
+  res.locals.error=req.app.get('env')==='development'? err:{};
 
   // render the error page
-  res.status(err.status || 500);
+  res.status(err.status||500);
   res.render('error');
 });
 
-module.exports = app;
+module.exports=app;
--- 5-NodeJS-Express-MongoDB/nucampsiteServer/routes/campsiteRouter.js
+++ 5-NodeJS-Express-MongoDB/nucampsiteServer/routes/campsiteRouter.js
@@ -177,7 +177,7 @@ campsiteRouter.route('/:campsiteId/comments/:commentId')
         Campsite.findById(req.params.campsiteId)
             .then(campsite => {
                 if (campsite&&campsite.comments.id(req.params.commentId)) {
-                    if ((campsite.comments.id(req.params.commentId).author).equals(req.user._id)) {
+                    if ((campsite.comments.id(req.params.commentId).author._id).equals(req.user._id)) {
                         if (req.body.rating) {
                             campsite.comments.id(req.params.commentId).rating=req.body.rating;
                         }
@@ -213,7 +213,7 @@ campsiteRouter.route('/:campsiteId/comments/:commentId')
         Campsite.findById(req.params.campsiteId)
             .then(campsite => {
                 if (campsite&&campsite.comments.id(req.params.commentId)) {
-                    if ((campsite.comments.id(req.params.commentId).author).equals(req.user._id)) {
+                    if ((campsite.comments.id(req.params.commentId).author._id).equals(req.user._id)) {
                         campsite.comments.id(req.params.commentId).remove();
                         campsite.save()
                             .then(campsite => {
